8ed5f2e125e2043ed64ade8beee172dcfa37d802
Md Imran <154124258+imoamo@users.noreply.github.com>
2024-09-28 11:14:21 +0530

    Add files via upload

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,9 +7,9 @@ const Hero = () => {
         <section className={styles.container}>
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi, I'm Imran</h1>
-                <p className={styles.description}>Full Stack Web Developer with 1.5+ years in HTML, CSS, JavaScript, React, MongoDB, Node.js, and Express.js. Connect to see what I can do!</p>
+                <p className={styles.description}>🌟✨ React Intern with 2+ years in HTML, CSS, JS, & React! 🚀 At Omniful building scalable solutions & crafting WOW user experiences! 🎨💖 Let’s create something amazing! 💥🤩</p>
 
-                <a href="https://resume-builder-test-new.masaischool.com/resume/public?resumeId=66a392316e6b499ee3362dbf" target='_blank' className={styles.contactBtn}>Resume</a>
+                <a href="https://resume-builder-test-new.masaischool.com/resume/public?resumeId=66f78b07f930ea373f9020a4" target='_blank' className={styles.contactBtn}>Resume</a>
             </div>
 
             <img src={getImageUrl("hero/heroImage.png")} alt='Hero image of me' className={styles.heroImg} />
